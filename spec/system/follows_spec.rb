0f7cbe04242578c74b1ff26dba7e056cba0c189f
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-25 17:24:29 +0900

    フォロイー/フォロワー一覧ページのテスト追加

--- spec/system/follows_spec.rb
+++ spec/system/follows_spec.rb
@@ -7,19 +7,47 @@ RSpec.describe 'Follows', type: :system do
 
   let(:user) { create(:user) }
 
-  describe 'フォローボタンのテスト' do
+  describe 'フォローボタンのテスト(ユーザーページ)' do
     before do
       sign_in
       visit user_path(user.nickname)
     end
 
     example 'フォロー出来ること' do
-      expect { click_on 'フォローする' }.to change { user.followers.count }.from(0).to(1)
+      expect { click_on 'フォローする' }.to change { user.followers.count }.by(1)
     end
 
     example 'フォロー解除できること' do
       click_on 'フォローする'
-      expect { click_on 'フォロー解除' }.to change { user.followers.count }.from(1).to(0)
+      expect { click_on 'フォロー解除' }.to change { user.followers.count }.by(-1)
+    end
+  end
+
+  describe 'フォローボタンのテスト(一覧ページ)' do
+    let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
+
+    before do
+      user2.follow(user)
+      user3.follow(user)
+      sign_in
+      visit user_followers_path(user.nickname)
+    end
+
+    example 'フォロー出来ること' do
+      within(".follow_form_#{user2.id}") { expect { click_on 'フォローする' }.to change { user2.followers.count }.by(1) }
+    end
+
+    example 'フォロー解除出来ること' do
+      within(".follow_form_#{user2.id}") do
+        click_on 'フォローする'
+        expect { click_on 'フォロー解除' }.to change { user2.followers.count }.by(-1)
+      end
+    end
+
+    example '特定のフォローボタンのみ切り替わっていること' do
+      within(".follow_form_#{user2.id}") { click_on 'フォローする' }
+      within(".follow_form_#{user3.id}") { expect(page).to have_button 'フォローする' }
     end
   end
 end
